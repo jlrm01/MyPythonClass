d99705ecb4c6cd993c479fe826202c7e197ab7ba
Jorge Rodriguez <jlrm01@gmail.com>
2016-02-03 02:28:24 -0400

    initial commit

---
+++ html/javascript/_vti_cnf/README.md
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|01 Dec 2015 23:12:48 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_cacheddtm:TX|01 Dec 2015 23:12:48 -0000
+vti_filesize:IR|4307
+vti_backlinkinfo:VX|
---
+++ html/javascript/_vti_cnf/highlight.pack.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 Jan 2016 01:54:40 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|AMIDARIS\\jlrm01
+vti_modifiedby:SR|AMIDARIS\\jlrm01
+vti_timecreated:TR|30 Jan 2016 01:54:40 -0000
+vti_cacheddtm:TX|30 Jan 2016 01:54:40 -0000
+vti_filesize:IR|8508
+vti_backlinkinfo:VX|default.html
